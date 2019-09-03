63ecb7b8728adec45f2dbd3f177d3dafd3f5e67d
rafaeltmbr <rafaeltmbr@gmail.com>
2019-09-03 12:33:02 -0300

    get back to the previous height state

--- src/script/menu-icon-height-fix.js
+++
@@ -1,9 +0,0 @@
-function resizeMenuBar() {
-    const height = window.innerHeight;
-    let menu = document.querySelector('.menu-icons');
-    menu.style.height = height + 'px';
-    console.log('height:', height);
-}
-
-window.addEventListener('load', resizeMenuBar);
-window.addEventListener('resize', resizeMenuBar);
