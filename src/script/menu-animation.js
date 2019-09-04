20be9fe6cdc3e854225cd9f50e1b890c4de6d7a1
rafaeltmbr <rafaeltmbr@gmail.com>
2019-09-04 00:33:21 -0300

    add borders around the menu list header

--- src/script/menu-animation.js
+++ src/script/menu-animation.js
@@ -1,7 +1,16 @@
 function animateHeaders() {
     const headers = document.getElementsByTagName('header');
     const k = Object.keys(headers);
-    k.map( h => headers[h].addEventListener('click', switchListAroundHeader));
+    k.map( h => headers[h].addEventListener('click', (event) => {
+        hideSameLevelList(event);
+        switchListAroundHeader(event);
+    }));
+}
+
+function hideSameLevelList({target: header}) {
+    const sibling = header.parentElement && header.parentElement.children;
+    /* Hide same level elements, but still save the current list state */
+
 }
 
 function switchListAroundHeader({target: header}) {
@@ -9,7 +18,7 @@ function switchListAroundHeader({target: header}) {
     if (sibling) {
         const keys = Object.keys(sibling);
         keys.map(k => {
-            if (sibling[k].tagName.toLowerCase() === 'ul' || sibling[k].tagName.toLowerCase() === 'li') {
+            if (sibling[k].tagName.toLowerCase() === 'ul' || sibling[k].tagName.toLowerCase() === 'ol') {
                 const display = window.getComputedStyle(sibling[k]).display;
                 sibling[k].style.display = ( display === 'none' ? 'block' : 'none');
             }
